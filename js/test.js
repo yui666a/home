cc206892a834035a359700735637181c090e1add
yui666a <haiso666@gmail.com>
2021-12-17 14:58:34 +0900

    全てトップに表示されるように

--- js/test.js
+++ js/test.js
@@ -3,8 +3,20 @@ let abst_research = "現在<br />文書中の暗示的怒りの検出に関す�
 
 
 document.getElementById("PROFILE").onclick = function() {
-    document.getElementById("display").innerHTML = document.getElementById("profile").innerHTML;
-  };
-  document.getElementById("RESEARCH").onclick = function() {
-    document.getElementById("display").innerHTML = document.getElementById("research").innerHTML;
-  };
+  document.getElementById("display").innerHTML = document.getElementById("profile").innerHTML;
+};
+document.getElementById("RESEARCH").onclick = function() {
+  document.getElementById("display").innerHTML = document.getElementById("research").innerHTML;
+};
+document.getElementById("PORTFOLIO").onclick = function() {
+  document.getElementById("display").innerHTML = document.getElementById("portfolio").innerHTML;
+};
+document.getElementById("SKILL").onclick = function() {
+  document.getElementById("display").innerHTML = document.getElementById("skill").innerHTML;
+};
+document.getElementById("PASTIME").onclick = function() {
+  document.getElementById("display").innerHTML = document.getElementById("pastime").innerHTML;
+};
+document.getElementById("CONTACT").onclick = function() {
+  document.getElementById("display").innerHTML = document.getElementById("contact").innerHTML;
+};
