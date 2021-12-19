5d19df6275b0e601c9a2077c1853017e3d142e3a
yui666a <haiso666@gmail.com>
2021-12-19 17:23:45 +0900

    spanうちまくり

--- js/test.js
+++ js/test.js
@@ -1,7 +1,3 @@
-let abst_research = "現在<br />文書中の暗示的怒りの検出に関する研究<br />高専時代<br />VRを用いた半導体教育に関する研究<br />";
-
-
-
 document.getElementById("PROFILE").onclick = function() {
   document.getElementById("display").innerHTML = document.getElementById("profile").innerHTML;
 };
